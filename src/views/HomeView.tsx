28d2e8044d9d8506eccc8ccabd288b6a048018f5
Valentin Acosta <chinisimooo@gmail.com>
2025-01-11 19:52:26 -0600

    redirect route from home view to profile view in case loged

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,30 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
 import Header from "../components/Header";
 import SearchForm from "../components/SearchForm";
+import { getUser } from "../api/DevTreeAPI";
+import { Navigate } from "react-router-dom";
 
 type Props = {};
 
 function HomeView({}: Props) {
-  return (
-    <>
-      <Header />
-      <main className="bg-gray-100 py-10 min-h-screen bg-no-repeat bg-right-top lg:bg-home lg:bg-home-xl">
-        <div className="max-w-5xl mx-auto mt-10">
-          <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
-            <h1 className="text-6xl font-black">
-              Todas tus <span className="text-cyan-400">Redes Sociales</span> en
-              un enlace
-            </h1>
-            <p className="text-slate-800 text-xl">
-              Únete a mas de 200 mil developers compartiendo sus redes sociales,
-              comparte tu perfil de TikTok, Facebook, Instagram, Youtube, Github
-              y más
-            </p>
-            <SearchForm />
+  const { data, isLoading, isError } = useQuery({
+    queryFn: getUser,
+    queryKey: ["user"],
+    retry: 2,
+    refetchOnWindowFocus: false,
+  });
+  if (isError || isLoading) {
+    return (
+      <>
+        <Header />
+        <main className="bg-gray-100 py-10 min-h-screen bg-no-repeat bg-right-top lg:bg-home lg:bg-home-xl">
+          <div className="max-w-5xl mx-auto mt-10">
+            <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
+              <h1 className="text-6xl font-black">
+                Todas tus <span className="text-cyan-400">Redes Sociales</span>{" "}
+                en un enlace
+              </h1>
+              <p className="text-slate-800 text-xl">
+                Únete a mas de 200 mil developers compartiendo sus redes
+                sociales, comparte tu perfil de TikTok, Facebook, Instagram,
+                Youtube, Github y más
+              </p>
+              <SearchForm />
+            </div>
           </div>
-        </div>
-      </main>
-    </>
-  );
+        </main>
+      </>
+    );
+  }
+  if (data) return <Navigate to={"/admin"} />;
 }
 
 export default HomeView;
